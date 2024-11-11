a6364cdff8bd8b31cd81bf5baf45182d6f2a2981
aleksandrekas <154304980+aleksandrekas@users.noreply.github.com>
2024-11-11 16:23:11 +0400

    added small changes

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -43,15 +43,15 @@ export default function DrumMachine(){
 
     return <div id="drum-machine">
             <div id="display">
-                <DrumPad disableStatus={powerBtn.disable}  action={handlePlayAudio} char={"Q"}  clipName={"Heater 1"} audio={"public/Heater-1.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"W"} clipName={"Heater 2"} audio={"public/Heater-2.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"E"} clipName={"Heater 3"} audio={"public/Heater-3.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"A"} clipName={"Heater 4"} audio={"public/Heater-4.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"S"} clipName={"Clap"} audio={"public/Clap.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"D"} clipName={"Open HH"} audio={"public/Open-HH.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"Z"} clipName={"Kick n'Hat"} audio={"public/Kick_n_Hat.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable}  action={handlePlayAudio} char={"Q"}  clipName={"Heater 1"} audio={"/Heater-1.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"W"} clipName={"Heater 2"} audio={"/Heater-2.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"E"} clipName={"Heater 3"} audio={"/Heater-3.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"A"} clipName={"Heater 4"} audio={"/Heater-4.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"S"} clipName={"Clap"} audio={"/Clap.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"D"} clipName={"Open HH"} audio={"/Open-HH.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"Z"} clipName={"Kick n'Hat"} audio={"/Kick_n_Hat.mp3"}/>
                 <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"X"} clipName={"Kick"} audio={"public/Kick.mp3"}/>
-                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"C"} clipName={"Closed HH"} audio={"public/Closed-HH.mp3"}/>
+                <DrumPad disableStatus={powerBtn.disable} action={handlePlayAudio} char={"C"} clipName={"Closed HH"} audio={"/Closed-HH.mp3"}/>
             </div>
             <div id="left-display">
             <h4>Power</h4>
